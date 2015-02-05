966b0662475f2c5e0aa72568173b5abb0966dc42
Ryan D'souza <dsouzarc@gmail.com>
2015-02-05 18:34:44 -0500

    Initial Commit

---
+++ HypnoNerd/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HypnoNerd
+//
+//  Created by Ryan D'souza on 2/5/15.
+//  Copyright (c) 2015 Ryan D'souza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
