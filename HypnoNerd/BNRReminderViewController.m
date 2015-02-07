6a4b040ac7f35f925f291866c78771b4bb499946
Ryan D'souza <dsouzarc@gmail.com>
2015-02-06 20:46:32 -0500

    New ViewController

---
+++ HypnoNerd/BNRReminderViewController.m
@@ -0,0 +1,45 @@
+//
+//  BNRReminderViewController.m
+//  HypnoNerd
+//
+//  Created by Ryan D'souza on 2/5/15.
+//  Copyright (c) 2015 Ryan D'souza. All rights reserved.
+//
+
+#import "BNRReminderViewController.h"
+
+@interface BNRReminderViewController ()
+
+@property (nonatomic, weak) IBOutlet UIDatePicker *datePicker;
+
+@end
+
+@implementation BNRReminderViewController
+
+- (IBAction)addReminder:(id)sender
+{
+    NSDate *dateChosen = self.datePicker.date;
+    NSLog(@"%@ was chosen", dateChosen);
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
